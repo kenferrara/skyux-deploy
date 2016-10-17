2167f45ea8af124bff92809d902513c96ff3b876
Bobby Earl <bobby.earl@blackbaud.com>
2016-10-17 14:29:03 -0400

    Last bit of coverage

--- test/lib-azure.spec.js
+++ test/lib-azure.spec.js
@@ -4,22 +4,52 @@
 describe('sky-pages-deploy lib azure', () => {
 
   const mock = require('mock-require');
-  const azure = require('azure-storage');
+  // const azure = require('azure-storage');
+  // const utils = require('../lib/utils');
+  const logger = require('winston');
 
   let lib;
   let createBlobServiceArgs;
   let createTableServiceArgs;
+  let createBlockBlobFromTextArgs;
+  let createContainerIfNotExistsArgs;
+  let createTableIfNotExistsArgs;
+  let insertOrReplaceEntityArgs;
 
   beforeEach(() => {
 
     createBlobServiceArgs = {};
     createTableServiceArgs = {};
+    createBlockBlobFromTextArgs = {};
+    createContainerIfNotExistsArgs = {};
+    createTableIfNotExistsArgs = {};
+    insertOrReplaceEntityArgs = {};
 
     mock('azure-storage', {
       createBlobService: (account, key) => {
         createBlobServiceArgs = {
           account: account,
           key: key
+        };
+
+        return {
+          createContainerIfNotExists: (blobName, acl, cb) => {
+            createContainerIfNotExistsArgs = {
+              blobName: blobName,
+              acl: acl,
+              cb: cb
+            };
+          },
+
+          createBlockBlobFromText: (blobName, assetName, assetContent, options, cb) => {
+            createBlockBlobFromTextArgs = {
+              blobName: blobName,
+              assetName: assetName,
+              assetContent: assetContent,
+              options: options,
+              cb: cb
+            };
+          }
         };
       },
 
@@ -29,6 +59,23 @@ describe('sky-pages-deploy lib azure', () => {
         createTableServiceArgs = {
           account: account,
           key: key
+        };
+
+        return {
+          createTableIfNotExists: (tableName, cb) => {
+            createTableIfNotExistsArgs = {
+              tableName: tableName,
+              cb: cb
+            };
+          },
+
+          insertOrReplaceEntity: (tableName, entity, cb) => {
+            insertOrReplaceEntityArgs = {
+              tableName: tableName,
+              entity: entity,
+              cb: cb
+            };
+          }
         };
       },
 
@@ -59,24 +106,98 @@ describe('sky-pages-deploy lib azure', () => {
   });
 
   describe('registerAssetsToBlob', () => {
-    it('should create a blob service using the supplied credentials', () => {
-      lib.registerAssetsToBlob({
-        azureStorageAccount: 'account1',
-        azureStorageAccessKey: 'key1'
-      });
-      expect(createBlobServiceArgs.account).toEqual('account1');
-      expect(createBlobServiceArgs.key).toEqual('key1');
+
+    it('should call createContainerIfNotExist and handle error', () => {
+      spyOn(logger, 'error');
+
+      const settings = { blobName: 'blob-name1' };
+      lib.registerAssetsToBlob(settings, []);
+      createContainerIfNotExistsArgs.cb('error1');
+      expect(logger.error).toHaveBeenCalledWith('error1');
+
     });
+
+    it('should call createContainerIfNotExist and handle success', () => {
+      spyOn(logger, 'info');
+
+      const settings = { blobName: 'blob-name2' };
+      const assets = [{
+        name: 'asset-name1',
+        content: 'asset-content1'
+      }];
+      lib.registerAssetsToBlob(settings, assets);
+      createContainerIfNotExistsArgs.cb();
+
+      expect(logger.info).toHaveBeenCalled();
+      expect(createBlockBlobFromTextArgs.blobName).toEqual(settings.name);
+      expect(createBlockBlobFromTextArgs.assetName).toEqual(assets[0].name);
+      expect(createBlockBlobFromTextArgs.assetContent).toEqual(assets[0].content);
+      expect(createBlockBlobFromTextArgs.options.contentSettings.contentType).toEqual(
+        'application/x-javascript'
+      );
+
+    });
+
+    it('should call createBlockBlobFromText and handle error', () => {
+
+      const settings = { blobName: 'blob-name2' };
+      const assets = [{
+        name: 'asset-name1',
+        content: 'asset-content1'
+      }];
+
+      spyOn(logger, 'error');
+      lib.registerAssetsToBlob(settings, assets);
+      createContainerIfNotExistsArgs.cb();
+      createBlockBlobFromTextArgs.cb('error2');
+      expect(logger.error).toHaveBeenCalledWith('error2');
+
+    });
+
   });
 
   describe('registerEntityToTable', () => {
+
     it('should create a table service using the supplied credentials', () => {
-      lib.registerEntityToTable({
+      const settings = {
         azureStorageAccount: 'account2',
         azureStorageAccessKey: 'key2'
-      });
-      expect(createTableServiceArgs.account).toEqual('account2');
-      expect(createTableServiceArgs.key).toEqual('key2');
+      };
+      lib.registerEntityToTable(settings);
+      expect(createTableServiceArgs.account).toEqual(settings.azureStorageAccount);
+      expect(createTableServiceArgs.key).toEqual(settings.azureStorageAccessKey);
     });
+
+    it('should call createTableIfNotExists and handle error', () => {
+      spyOn(logger, 'error');
+
+      lib.registerEntityToTable({}, {});
+      createTableIfNotExistsArgs.cb('error3');
+      expect(logger.error).toHaveBeenCalledWith('error3');
+    });
+
+    it('should call insertOrReplaceEntity and handle success', () => {
+      const settings = { name: 'custom-name3' };
+
+      spyOn(logger, 'info');
+      lib.registerEntityToTable(settings, {});
+      createTableIfNotExistsArgs.cb();
+      insertOrReplaceEntityArgs.cb();
+
+      expect(logger.info).toHaveBeenCalledWith(
+        'SPA %s registered in table storage.',
+        settings.name
+      );
+
+    });
+
+    it('should call insertOrReplaceEntity and handle error', () => {
+      spyOn(logger, 'error');
+      lib.registerEntityToTable({}, {});
+      createTableIfNotExistsArgs.cb();
+      insertOrReplaceEntityArgs.cb('error4');
+      expect(logger.error).toHaveBeenCalledWith('error4');
+    });
+
   });
 });
